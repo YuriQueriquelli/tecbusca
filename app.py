0a25d0e1b7b2933afd95bd9b3e7ac8b523ffbec8
Yuri Queriquelli <yurirafael@outlook.com>
2021-11-01 09:10:46 -0300

    Sistema de listagem e exibição de vagas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -6,15 +6,31 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:sensedata@localhost:5432/tg'
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+class Vaga_geral(db.Model):
+    geral_url = db.Column(db.String(255), primary_key=True)
+    geral_titulo = db.Column(db.String)
+    geral_cargo = db.Column(db.String)
+    geral_desc = db.Column(db.Text)
+    geral_data = db.Column(db.DateTime)
+    curso_id = db.Column(db.Integer, db.ForeignKey('curso.id'))
+
+class Curso(db.Model):
+    curso_id = db.Column(db.String, primary_key=True)
+    curso_titulo = db.Column(db.String(50))
+
 
 @app.route('/', methods=['GET','POST'])
 def index():
     return render_template('index.html')
 
+
 @app.route('/vagas', methods=['GET', 'POST'])
 def vagas():
-    return render_template('vagas.html')
+    vagas = Vaga_geral.query.filter().all()
+    return render_template('vagas.html', vagas=vagas)
+
 
 if __name__ == '__main__':
     app.run(port='9090', debug=True)
